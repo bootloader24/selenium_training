542fc8616f799c1c8ea1d90e71b51556903e363d
Artem.Butin <Artem.Butin@infotecs.ru>
2024-01-30 19:42:03 +0500

    Исправление задания 9
    Вместо получения текста через getText() из "невидимой" опции элемента select задействован метод getAccessibleName(), получающий видимый текст из соответствующей ячейки таблицы

--- litecart-tests/src/test/java/ru/training/litecart/tests/GeozonesTests.java
+++ litecart-tests/src/test/java/ru/training/litecart/tests/GeozonesTests.java
@@ -8,6 +8,8 @@ import ru.training.litecart.common.CommonFunctions;
 
 import java.util.List;
 
+import static java.util.stream.Collectors.toList;
+
 public class GeozonesTests extends TestBase {
 
     @Test
@@ -28,12 +30,12 @@ public class GeozonesTests extends TestBase {
         for (var link : zonesLinks) {
             app.driver.get(link);
             // после перехода на страницу с таблицей геозон извлекаем элементы выбранных в каждой строке зон:
-            var selects = app.driver.findElements(By.cssSelector("#table-zones td:nth-child(3) select > [selected]"));
+            var selects = app.driver.findElements(By.cssSelector("#table-zones td:nth-child(3)"));
 
             // из списка элементов с выбранными зонами формируем список строк с названиями зон
             List<String> geozonesList = selects
                     .stream()
-                    .map(WebElement::getText)
+                    .map(WebElement::getAccessibleName)
                     .toList();
             // проверяем, что список с названиями зон корректно отсортирован:
             Assertions.assertTrue(CommonFunctions.checkSorting(geozonesList));
